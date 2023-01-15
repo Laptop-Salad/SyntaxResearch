0ba862883bd3a14188d0731ccc74c07076568d8b
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 11:44:48 +0000

    Create ten.java

---
+++ java/ten.java
@@ -0,0 +1,10 @@
+class Animals {
+    public static void main(String[] args) {
+        String animal = "cat";
+        if (animal == "cat") {
+            System.out.println("Gives cat fish");
+        } else {
+            System.out.println("I dont know what that is");
+        }
+    }
+}
