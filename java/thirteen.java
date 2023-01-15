ef338a7d8a3d0cc9a013efe61664a5817e49b606
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 11:53:57 +0000

    Create thirteen.java

---
+++ java/thirteen.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Input {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        String message = myScanner.nextLine();
+        System.out.println(message);
+    }
+}
