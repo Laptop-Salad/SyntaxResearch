f37bb00304150508fb5739f9756fabfcd6da0e42
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 11:45:53 +0000

    Update ten.java

--- java/ten.java
+++ java/ten.java
@@ -2,7 +2,7 @@ class Animals {
     public static void main(String[] args) {
         String animal = "cat";
         if (animal == "cat") {
-            System.out.println("Gives cat fish");
+            System.out.println("Gives cat a fish");
         } else {
             System.out.println("I dont know what that is");
         }
