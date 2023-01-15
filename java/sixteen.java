2ce10c7d35a04f24fb9e90a6d5bd09e42182e440
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 13:09:52 +0000

    Create sixteen.java

---
+++ java/sixteen.java
@@ -0,0 +1,12 @@
+class Fruit {
+    String colour = "None";
+    String name = "None";
+}
+
+class Main {
+    public static void main(String[] args) {
+        Fruit banana = new Fruit();
+        banana.colour = "yellow";
+        banana.name = "banana";
+    }
+}
