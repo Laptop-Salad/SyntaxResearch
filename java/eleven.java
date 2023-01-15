9f690a6785bb2b1a63d8824786bcc1edc865b187
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 11:45:42 +0000

    Create eleven.java

---
+++ java/eleven.java
@@ -0,0 +1,12 @@
+class Animals {
+    public static void main(String[] args) {
+        String animal = "cat";
+        if (animal == "cat") {
+            System.out.println("Gives cat a fish");
+        } else if (animal == "dog") {
+            System.out.println("Gives dog a bone");
+        } else {
+            System.out.println("I dont know what that is");
+        }
+    }
+}
