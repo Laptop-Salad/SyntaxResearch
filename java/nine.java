9c64d734756770596406fd87f746616cf1b96626
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 11:46:03 +0000

    Update nine.java

--- java/nine.java
+++ java/nine.java
@@ -2,7 +2,7 @@ class Cat {
     public static void main(String[] args) {
         String animal = "cat";
         if (animal == "cat") {
-            System.out.println("Gives cat fish");
+            System.out.println("Gives cat a fish");
         }
     }
 }
