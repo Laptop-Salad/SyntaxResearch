07eed2d47acdb33dff45d40e155e819a10a3fd51
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 11:42:59 +0000

    Create nine.java

---
+++ java/nine.java
@@ -0,0 +1,8 @@
+class Modulus {
+    public static void main(String[] args) {
+        String animal = "cat";
+        if (animal == "cat") {
+            System.out.println("Gives cat fish");
+        }
+    }
+}
