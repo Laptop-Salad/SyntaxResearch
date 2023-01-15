bfcbd681b277dc0a6c6f59636ecd98e5ee1499bb
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 13:13:43 +0000

    Create nineteen.java

---
+++ java/nineteen.java
@@ -0,0 +1,9 @@
+class Main {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(add(5, 3));
+    }
+}
