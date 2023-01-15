b6d93d7a0bf3d35541efb72931bfbee123970457
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 12:07:36 +0000

    Update thirteen.java

--- java/thirteen.java
+++ java/thirteen.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Input {
+class Main {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         String message = myScanner.nextLine();
