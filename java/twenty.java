aa570fa17aa055a1e426b08d7db0dbe0b0ce0ce6
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 13:18:59 +0000

    Create twenty.java

---
+++ java/twenty.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        boolean isHappy = true;
+        boolean isSad = false;
+        System.out.println(isHappy);
+        System.out.println(isSad);
+    }
+}
