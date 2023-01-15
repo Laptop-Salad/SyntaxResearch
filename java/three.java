c2ddaaddd142a636bc9b2315dbcc83a5e4d322d8
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-15 11:37:21 +0000

    Create three.java

---
+++ java/three.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String string_one = "Hello";
+        String string_two = "world";
+        System.out.println(string_one + string_two);
+    }
+}
