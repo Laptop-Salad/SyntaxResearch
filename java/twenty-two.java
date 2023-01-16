c085bdb1833a6328c45f1ebeef43e313643bad47
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-16 08:33:07 +0000

    Create twenty-two.java

---
+++ java/twenty-two.java
@@ -0,0 +1,9 @@
+class Main {
+    static int subtract(int x, int y) {
+        return x - y;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(subtract(5, 3));
+    }
+}
