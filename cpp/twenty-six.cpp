466267812ab1c7a95a7a58227032ada25b8bca12
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-16 12:45:13 +0000

    Create twenty-six.cpp

---
+++ cpp/twenty-six.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void helloWorld() {
+    cout << "Hello world.";
+}
+
+int main() {
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    return 0;
+}
