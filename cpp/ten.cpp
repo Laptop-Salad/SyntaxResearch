c001160db94b889536e484a8b632f3a9e1ac4df2
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-16 12:29:04 +0000

    Create ten.cpp

---
+++ cpp/ten.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string animal = "cat";
+    
+    if (animal == "cat") {
+        cout << "Gives cat a fish";
+    } else {
+        cout << "I dont know what that is";
+    }
+    
+    return 0;
+}
