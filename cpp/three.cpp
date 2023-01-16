8ddb36bd54cafa0bf49d6725f3c291f194139885
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-16 09:03:04 +0000

    Create three.cpp

---
+++ cpp/three.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string messageOne = "Hello";
+    string messageTwo = "world.";
+    cout << messageOne + messageTwo;
+
+    return 0;
+}
