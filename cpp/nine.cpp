d7e327708ceac52bd9869ce997abf23500f6f4a8
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-16 12:28:31 +0000

    Create nine.cpp

---
+++ cpp/nine.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string animal = "cat";
+    
+    if (animal == "cat") {
+        cout << "Gives cat a fish";
+    }
+    
+    return 0;
+}
