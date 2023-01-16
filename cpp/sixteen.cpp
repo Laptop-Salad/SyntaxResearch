d09dbcf6886f8210cc1023bcde51d09ce3ae28d9
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-16 12:35:59 +0000

    Create sixteen.cpp

---
+++ cpp/sixteen.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Fruit {
+    public:
+        string colour;
+        string name;
+};
+
+int main() {
+    Fruit banana;
+    banana.colour = "yellow";
+    banana.name = "banana";
+    return 0;
+}
