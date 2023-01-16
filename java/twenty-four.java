01b107f15eb40da8f2356ce6e21a30c2d2c60b47
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-16 08:36:00 +0000

    Create twenty-four.java

---
+++ java/twenty-four.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        if (7 > 8) {
+            System.out.println("7 is greater than 8");
+        } else {
+            System.out.println("7 is not greater than 8");
+        }
+    }
+}
