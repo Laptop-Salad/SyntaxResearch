e2c38e149a9226b460d6eea188891cdf1e8fd80c
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-01-16 12:43:56 +0000

    Create twenty-four.cpp

---
+++ cpp/twenty-four.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    if (7 > 8) {
+        cout << "7 is greater than 8";
+    } else {
+        cout << "7 is not greater than 8";
+    }
+    
+    return 0;
+}
